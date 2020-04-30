4c0f35c605b1346ae59987adea23c48a7b43b5b8
Yunrou <tao86419@gmail.com>
2020-04-30 11:02:32 +0800

    final version

--- README.md
+++ README.md
@@ -3,8 +3,11 @@ A term project of AI course
 
 ## How to Compile and Run
 - Python version: 3.7.4 <br>
-  Be sure of installing dependencies for all packages:
-    - numpy==1.18.3
+  Be sure of installing and upgrading dependencies for all packages:
+    - numpy==<font color=firebrick>1.18.3</font><br> 
+      ```
+      pip install --upgrade numpy
+      ```
 - Quick start<br>
   ```
   $ python game.py
--- game.py
+++ game.py
@@ -17,78 +17,82 @@ def max_depth(boardsize, AI_first):
     else:
         if AI_first: return 22 
         else: return 21 
+def main():
 
-os.system("")
-
-while True:
-    
-    # a.
-    turn = int(input("User first? (0/1): ").split(' ')[0])
-    if turn not in (0, 1):
-        continue
-    
-    # b. Initialize board
-    boardsize = int(input("Board Size? (4 or 6): ").split(' ')[0])
-    if boardsize not in (4, 6):
-        continue
-    
-
-    board = np.zeros((boardsize, boardsize), dtype=np.int8)
-    # c. Initialized cards
-    cards = CARDS[boardsize].copy()
-
-    display_board(board)
-    display_cards(cards)
-    
-    depth = max_depth(boardsize, not turn)
+    os.system("")
 
     while True:
-        print("")
-        if turn == 1:
-            # user turn
-            while True:
-                row, col, weight = [int(i) for i in input("Input (row, col, weight): ").split(' ')[:3]]
-
-                next_state = place_chess(False, board.copy(), cards.copy(), 
-                                         row, col, -weight)
-                if isinstance(next_state, tuple):
-                    (board, cards) = next_state 
-                    break
-                print("Oops! Wrong input ...")
-
-            print("\n\x1b[92m[User]:\x1b[0m ("+str(row)+", "+str(col)+", "+str(weight)+")")
-
-            display_board(board)
-            display_cards(cards)
-
-            turn = 0
-        else:
-
-            agent = MinimaxAgent(board.copy(), cards.copy(), depth, TIMEOUT)
-            # t = Timer(TIMEOUT, print, ['Sorry, times up'])
-            # t.start()
-            # choose the bestmove
-            (row, col, weight) = agent.move()
-            # t.cancel()
-            (board, cards) = place_chess(True, board.copy(), cards.copy(), 
-                                         row, col, weight)
-            print("\x1b[93m[AI]:\x1b[0m ("+str(row)+", "+str(col)+", "+str(weight)+")")
-            # B.check()
-            display_board(board)
-            display_cards(cards)
-            
-            depth -= 2
-            turn = 1
+        
+        # a.
+        turn = int(input("User first? (0/1): ").split(' ')[0])
+        if turn not in (0, 1):
+            continue
+        
+        # b. Initialize board
+        boardsize = int(input("Board Size? (4 or 6): ").split(' ')[0])
+        if boardsize not in (4, 6):
+            continue
         
 
-        if not len(cards): # empty
-            win = win_check(board)
-            if win == 'user':
-                print('User Win')
-            elif win == 'ai':
-                print('AI Win')
-            else:
-                print('Tie')
-            break
+        board = np.zeros((boardsize, boardsize), dtype=np.int8)
+        # c. Initialized cards
+        cards = CARDS[boardsize].copy()
 
-    break
+        display_board(board)
+        display_cards(cards)
+        
+        depth = max_depth(boardsize, not turn)
+
+        while True:
+            print("")
+            if turn == 1:
+                # user turn
+                while True:
+                    row, col, weight = [int(i) for i in input("Input (row, col, weight): ").split(' ')[:3]]
+
+                    next_state = place_chess(False, board.copy(), cards.copy(), 
+                                             row, col, -weight)
+                    if isinstance(next_state, tuple):
+                        (board, cards) = next_state 
+                        break
+                    print("Oops! Wrong input ...")
+
+                print("\n\x1b[92m[User]:\x1b[0m ("+str(row)+", "+str(col)+", "+str(weight)+")")
+
+                display_board(board)
+                display_cards(cards)
+
+                turn = 0
+            else:
+
+                agent = MinimaxAgent(board.copy(), cards.copy(), depth, TIMEOUT)
+                # t = Timer(TIMEOUT, print, ['Sorry, times up'])
+                # t.start()
+                # choose the bestmove
+                (row, col, weight) = agent.move()
+                # t.cancel()
+                (board, cards) = place_chess(True, board.copy(), cards.copy(), 
+                                             row, col, weight)
+                print("\x1b[93m[AI]:\x1b[0m ("+str(row)+", "+str(col)+", "+str(weight)+")")
+                # B.check()
+                display_board(board)
+                display_cards(cards)
+                
+                depth -= 2
+                turn = 1
+            
+
+            if not len(cards): # empty
+                win = win_check(board)
+                if win == 'user':
+                    print('User Win')
+                elif win == 'ai':
+                    print('AI Win')
+                else:
+                    print('Tie')
+                break
+
+        break
+
+if __name__ == '__main__':
+    main()
